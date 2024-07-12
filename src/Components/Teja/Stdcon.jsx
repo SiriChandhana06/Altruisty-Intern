495ca0a0d92db832d9168653c4f01c993c81b080
Tejasri-R <tejasri27dinesh@gmail.com>
2024-07-12 15:23:07 +0530

    commit

--- src/Components/Teja/Stdcon.jsx
+++ src/Components/Teja/Stdcon.jsx
@@ -4,7 +4,11 @@ import Section from './Section';
 const StudentContributionPage = ({ videos }) => {
   return (
     <>
-      <h1 className='flex justify-center font-bold text-4xl pt-6 pb-2 border-[#eeeeee] border-b-2 mx-10 shadow-lg'>Student Contribution Stories</h1>
+    <h1 className='text-center xl:text-center xl:text-4xl sm:text-left font-bold text-l sm:text-l pt-6 pb-2 border-[#eeeeee] border-b-2 mx-10 shadow-lg'>
+  Student Contribution Stories
+</h1>
+
+     
       <div  className='mx-20'>
       <Section videos={videos} />
       <Section videos={videos} />
