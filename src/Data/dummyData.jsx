765d292e4c816643755c91712b89abac597f85f5
SiriChandhana06 <sirisiri3006@gmail.com>
2024-07-17 21:08:27 +0530

    data

--- src/Data/dummyData.jsx
+++ src/Data/dummyData.jsx
@@ -58,71 +58,71 @@ const data = [
     {
         id: '1',
         title: 'Startup Journey: From Idea to Reality',
-        src: 'video1.mp4',
-        description: 'Description of Startup Journey',
+        src: 'https://drive.google.com/file/d/13nLwAM7nj8tEsRTxB5uvrSqqFGRlqnWI/view?usp=drive_link',
+        description: 'Description of Startup Journey, Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi, velit assumenda? Sed dolores quod natus fugiat dolor tenetur assumenda provident eaque voluptates officiis cumque possimus eos, voluptatum sunt. Corrupti, deleniti?',
         img: 'link_to_image_1'
     },
     {
         id: '2',
         title: 'Success Story: Overcoming Obstacles',
-        src: 'video2.mp4',
-        description: 'Description of Success Story',
+        src: 'https://drive.google.com/file/d/1SSXp5NMZYgjQH5aEAafRsgEsmiAEZmg8/view?usp=drive_link',
+        description: 'Description of Success Story, Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi, velit assumenda? Sed dolores quod natus fugiat dolor tenetur assumenda provident eaque voluptates officiis cumque possimus eos, voluptatum sunt. Corrupti, deleniti?',
         img: 'link_to_image_2'
     },
     {
         id: '3',
         title: 'Learning from Failure: A Case Study',
-        src: 'video3.mp4',
-        description: 'Description of Learning from Failure',
+        src: 'https://drive.google.com/file/d/12lPN6TwmqINzxkc9g1oBjyhHT6AgXvt6/view?usp=drive_link',
+        description: 'Description of Learning from Failure, Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi, velit assumenda? Sed dolores quod natus fugiat dolor tenetur assumenda provident eaque voluptates officiis cumque possimus eos, voluptatum sunt. Corrupti, deleniti?',
         img: 'link_to_image_3'
     },
     {
         id: '4',
         title: 'Podcast: Expert Advice for Startups',
-        src: 'video1.mp4',
-        description: 'Description of Podcast: Expert Advice',
+        src: 'https://drive.google.com/file/d/13nLwAM7nj8tEsRTxB5uvrSqqFGRlqnWI/view?usp=drive_link',
+        description: 'Description of Podcast: Expert Advice, Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi, velit assumenda? Sed dolores quod natus fugiat dolor tenetur assumenda provident eaque voluptates officiis cumque possimus eos, voluptatum sunt. Corrupti, deleniti?',
         img: 'link_to_image_4'
     },
     {
         id: '5',
         title: 'Building a Dream Team',
-        src: 'video2.mp4',
-        description: 'Description of Building a Dream Team',
+        src: 'https://drive.google.com/file/d/1SSXp5NMZYgjQH5aEAafRsgEsmiAEZmg8/view?usp=drive_link',
+        description: 'Description of Building a Dream Team, Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi, velit assumenda? Sed dolores quod natus fugiat dolor tenetur assumenda provident eaque voluptates officiis cumque possimus eos, voluptatum sunt. Corrupti, deleniti?',
         img: 'link_to_image_5'
     },
     {
         id: '6',
         title: 'Startup Mentorship: Guidance from the Best',
-        src: 'video3.mp4',
-        description: 'Description of Startup Mentorship',
+        src: 'https://drive.google.com/file/d/12lPN6TwmqINzxkc9g1oBjyhHT6AgXvt6/view?usp=drive_link',
+        description: 'Description of Startup Mentorship, Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi, velit assumenda? Sed dolores quod natus fugiat dolor tenetur assumenda provident eaque voluptates officiis cumque possimus eos, voluptatum sunt. Corrupti, deleniti?',
         img: 'link_to_image_6'
     },
     {
         id: '7',
         title: 'Against All Odds: Inspiring Startup Stories',
-        src: 'video1.mp4',
-        description: 'Description of Against All Odds',
+        src: 'https://drive.google.com/file/d/13nLwAM7nj8tEsRTxB5uvrSqqFGRlqnWI/view?usp=drive_link',
+        description: 'Description of Against All Odds, Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi, velit assumenda? Sed dolores quod natus fugiat dolor tenetur assumenda provident eaque voluptates officiis cumque possimus eos, voluptatum sunt. Corrupti, deleniti?',
         img: 'link_to_image_7'
     },
     {
         id: '8',
         title: 'Podcast: The Future of Startups',
-        src: 'video2.mp4',
-        description: 'Description of Podcast: Future of Startups',
+        src: 'https://drive.google.com/file/d/1SSXp5NMZYgjQH5aEAafRsgEsmiAEZmg8/view?usp=drive_link',
+        description: 'Description of Podcast: Future of Startups, Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi, velit assumenda? Sed dolores quod natus fugiat dolor tenetur assumenda provident eaque voluptates officiis cumque possimus eos, voluptatum sunt. Corrupti, deleniti?',
         img: 'link_to_image_8'
     },
     {
         id: '9',
         title: 'Success in Startup: A Practical Guide',
-        src: 'video3.mp4',
-        description: 'Description of Success in Startup',
+        src: 'https://drive.google.com/file/d/12lPN6TwmqINzxkc9g1oBjyhHT6AgXvt6/view?usp=drive_link',
+        description: 'Description of Success in Startup, Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi, velit assumenda? Sed dolores quod natus fugiat dolor tenetur assumenda provident eaque voluptates officiis cumque possimus eos, voluptatum sunt. Corrupti, deleniti?',
         img: 'link_to_image_9'
     },
     {
         id: '10',
         title: 'Podcast: Marketing Strategies for Startups',
-        src: 'video1.mp4',
-        description: 'Description of Podcast: Marketing Strategies',
+        src: 'https://drive.google.com/file/d/13nLwAM7nj8tEsRTxB5uvrSqqFGRlqnWI/view?usp=drive_link',
+        description: 'Description of Podcast: Marketing Strategies, Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi, velit assumenda? Sed dolores quod natus fugiat dolor tenetur assumenda provident eaque voluptates officiis cumque possimus eos, voluptatum sunt. Corrupti, deleniti?',
         img: 'link_to_image_10'
     }
   ];
