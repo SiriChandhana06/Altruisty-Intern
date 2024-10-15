0e14cabc8c2623feace70d56cd0eb3298137e474
Siri Chandhana K <sirisiri3006@gmail.com>
2024-10-15 08:50:35 +0530

    Update dummyData.jsx

--- src/Data/dummyData.jsx
+++ src/Data/dummyData.jsx
@@ -8,47 +8,47 @@ const data = [
   {
     id: '2',
     title: 'Success Story: Overcoming Obstacles',
-    src: 'data/success_story.mp4'
+    src: '/video2.mp4'
   },
   {
     id: '3',
     title: 'Learning from Failure: A Case Study',
-    src: 'data/learning_from_failure.mp4'
+    src: '/video3.mp4'
   },
   {
     id: '4',
     title: 'Podcast: Expert Advice for Startups',
-    src: 'data/podcast_expert_advice.mp4'
+    src: '/video1.mp4'
   },
   {
     id: '5',
     title: 'Building a Dream Team',
-    src: 'data/building_dream_team.mp4'
+    src: '/video2.mp4'
   },
   {
     id: '6',
     title: 'Startup Mentorship: Guidance from the Best',
-    src: 'data/startup_mentorship.mp4'
+    src: '/video3.mp4'
   },
   {
     id: '7',
     title: 'Against All Odds: Inspiring Startup Stories',
-    src: 'data/against_all_odds.mp4'
+    src: '/video1.mp4'
   },
   {
     id: '8',
     title: 'Podcast: The Future of Startups',
-    src: 'data/podcast_future_of_startups.mp4'
+    src: '/video2.mp4'
   },
   {
     id: '9',
     title: 'Success in Startup: A Practical Guide',
-    src: 'data/success_in_startup.mp4'
+    src: '/video3.mp4'
   },
   {
     id: '10',
     title: 'Podcast: Marketing Strategies for Startups',
-    src: 'data/podcast_marketing_strategies.mp4'
+    src: '/video1.mp4'
   }
 ];
 
