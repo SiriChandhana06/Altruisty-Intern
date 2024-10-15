cc170555ffa95e4f0fc5a8b53b623f4e68c4adfc
Siri Chandhana K <sirisiri3006@gmail.com>
2024-10-15 08:45:07 +0530

    Update dummyData.jsx

--- src/Data/dummyData.jsx
+++ src/Data/dummyData.jsx
@@ -1,131 +1,131 @@
 
-// const data = [
-//   {
-//     id: '1',
-//     title: 'Startup Journey: From Idea to Reality',
-//     src: 'data/startup_journey.mp4'
-//   },
-//   {
-//     id: '2',
-//     title: 'Success Story: Overcoming Obstacles',
-//     src: 'data/success_story.mp4'
-//   },
-//   {
-//     id: '3',
-//     title: 'Learning from Failure: A Case Study',
-//     src: 'data/learning_from_failure.mp4'
-//   },
-//   {
-//     id: '4',
-//     title: 'Podcast: Expert Advice for Startups',
-//     src: 'data/podcast_expert_advice.mp4'
-//   },
-//   {
-//     id: '5',
-//     title: 'Building a Dream Team',
-//     src: 'data/building_dream_team.mp4'
-//   },
-//   {
-//     id: '6',
-//     title: 'Startup Mentorship: Guidance from the Best',
-//     src: 'data/startup_mentorship.mp4'
-//   },
-//   {
-//     id: '7',
-//     title: 'Against All Odds: Inspiring Startup Stories',
-//     src: 'data/against_all_odds.mp4'
-//   },
-//   {
-//     id: '8',
-//     title: 'Podcast: The Future of Startups',
-//     src: 'data/podcast_future_of_startups.mp4'
-//   },
-//   {
-//     id: '9',
-//     title: 'Success in Startup: A Practical Guide',
-//     src: 'data/success_in_startup.mp4'
-//   },
-//   {
-//     id: '10',
-//     title: 'Podcast: Marketing Strategies for Startups',
-//     src: 'data/podcast_marketing_strategies.mp4'
-//   }
-// ];
-
-// export default data;
-
 const data = [
-    {
-        id: '1',
-        title: 'Startup Journey: From Idea to Reality',
-        src: 'https://drive.google.com/file/d/13nLwAM7nj8tEsRTxB5uvrSqqFGRlqnWI/view?usp=drive_link',
-        description: 'Description of Startup Journey, Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi, velit assumenda? Sed dolores quod natus fugiat dolor tenetur assumenda provident eaque voluptates officiis cumque possimus eos, voluptatum sunt. Corrupti, deleniti?',
-        img: 'link_to_image_1'
-    },
-    {
-        id: '2',
-        title: 'Success Story: Overcoming Obstacles',
-        src: 'https://drive.google.com/file/d/1SSXp5NMZYgjQH5aEAafRsgEsmiAEZmg8/view?usp=drive_link',
-        description: 'Description of Success Story, Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi, velit assumenda? Sed dolores quod natus fugiat dolor tenetur assumenda provident eaque voluptates officiis cumque possimus eos, voluptatum sunt. Corrupti, deleniti?',
-        img: 'link_to_image_2'
-    },
-    {
-        id: '3',
-        title: 'Learning from Failure: A Case Study',
-        src: 'https://drive.google.com/file/d/12lPN6TwmqINzxkc9g1oBjyhHT6AgXvt6/view?usp=drive_link',
-        description: 'Description of Learning from Failure, Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi, velit assumenda? Sed dolores quod natus fugiat dolor tenetur assumenda provident eaque voluptates officiis cumque possimus eos, voluptatum sunt. Corrupti, deleniti?',
-        img: 'link_to_image_3'
-    },
-    {
-        id: '4',
-        title: 'Podcast: Expert Advice for Startups',
-        src: 'https://drive.google.com/file/d/13nLwAM7nj8tEsRTxB5uvrSqqFGRlqnWI/view?usp=drive_link',
-        description: 'Description of Podcast: Expert Advice, Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi, velit assumenda? Sed dolores quod natus fugiat dolor tenetur assumenda provident eaque voluptates officiis cumque possimus eos, voluptatum sunt. Corrupti, deleniti?',
-        img: 'link_to_image_4'
-    },
-    {
-        id: '5',
-        title: 'Building a Dream Team',
-        src: 'https://drive.google.com/file/d/1SSXp5NMZYgjQH5aEAafRsgEsmiAEZmg8/view?usp=drive_link',
-        description: 'Description of Building a Dream Team, Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi, velit assumenda? Sed dolores quod natus fugiat dolor tenetur assumenda provident eaque voluptates officiis cumque possimus eos, voluptatum sunt. Corrupti, deleniti?',
-        img: 'link_to_image_5'
-    },
-    {
-        id: '6',
-        title: 'Startup Mentorship: Guidance from the Best',
-        src: 'https://drive.google.com/file/d/12lPN6TwmqINzxkc9g1oBjyhHT6AgXvt6/view?usp=drive_link',
-        description: 'Description of Startup Mentorship, Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi, velit assumenda? Sed dolores quod natus fugiat dolor tenetur assumenda provident eaque voluptates officiis cumque possimus eos, voluptatum sunt. Corrupti, deleniti?',
-        img: 'link_to_image_6'
-    },
-    {
-        id: '7',
-        title: 'Against All Odds: Inspiring Startup Stories',
-        src: 'https://drive.google.com/file/d/13nLwAM7nj8tEsRTxB5uvrSqqFGRlqnWI/view?usp=drive_link',
-        description: 'Description of Against All Odds, Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi, velit assumenda? Sed dolores quod natus fugiat dolor tenetur assumenda provident eaque voluptates officiis cumque possimus eos, voluptatum sunt. Corrupti, deleniti?',
-        img: 'link_to_image_7'
-    },
-    {
-        id: '8',
-        title: 'Podcast: The Future of Startups',
-        src: 'https://drive.google.com/file/d/1SSXp5NMZYgjQH5aEAafRsgEsmiAEZmg8/view?usp=drive_link',
-        description: 'Description of Podcast: Future of Startups, Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi, velit assumenda? Sed dolores quod natus fugiat dolor tenetur assumenda provident eaque voluptates officiis cumque possimus eos, voluptatum sunt. Corrupti, deleniti?',
-        img: 'link_to_image_8'
-    },
-    {
-        id: '9',
-        title: 'Success in Startup: A Practical Guide',
-        src: 'https://drive.google.com/file/d/12lPN6TwmqINzxkc9g1oBjyhHT6AgXvt6/view?usp=drive_link',
-        description: 'Description of Success in Startup, Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi, velit assumenda? Sed dolores quod natus fugiat dolor tenetur assumenda provident eaque voluptates officiis cumque possimus eos, voluptatum sunt. Corrupti, deleniti?',
-        img: 'link_to_image_9'
-    },
-    {
-        id: '10',
-        title: 'Podcast: Marketing Strategies for Startups',
-        src: 'https://drive.google.com/file/d/13nLwAM7nj8tEsRTxB5uvrSqqFGRlqnWI/view?usp=drive_link',
-        description: 'Description of Podcast: Marketing Strategies, Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi, velit assumenda? Sed dolores quod natus fugiat dolor tenetur assumenda provident eaque voluptates officiis cumque possimus eos, voluptatum sunt. Corrupti, deleniti?',
-        img: 'link_to_image_10'
-    }
-  ];
+  {
+    id: '1',
+    title: 'Startup Journey: From Idea to Reality',
+    src: 'data/startup_journey.mp4'
+  },
+  {
+    id: '2',
+    title: 'Success Story: Overcoming Obstacles',
+    src: 'data/success_story.mp4'
+  },
+  {
+    id: '3',
+    title: 'Learning from Failure: A Case Study',
+    src: 'data/learning_from_failure.mp4'
+  },
+  {
+    id: '4',
+    title: 'Podcast: Expert Advice for Startups',
+    src: 'data/podcast_expert_advice.mp4'
+  },
+  {
+    id: '5',
+    title: 'Building a Dream Team',
+    src: 'data/building_dream_team.mp4'
+  },
+  {
+    id: '6',
+    title: 'Startup Mentorship: Guidance from the Best',
+    src: 'data/startup_mentorship.mp4'
+  },
+  {
+    id: '7',
+    title: 'Against All Odds: Inspiring Startup Stories',
+    src: 'data/against_all_odds.mp4'
+  },
+  {
+    id: '8',
+    title: 'Podcast: The Future of Startups',
+    src: 'data/podcast_future_of_startups.mp4'
+  },
+  {
+    id: '9',
+    title: 'Success in Startup: A Practical Guide',
+    src: 'data/success_in_startup.mp4'
+  },
+  {
+    id: '10',
+    title: 'Podcast: Marketing Strategies for Startups',
+    src: 'data/podcast_marketing_strategies.mp4'
+  }
+];
+
+export default data;
+
+// const data = [
+//     {
+//         id: '1',
+//         title: 'Startup Journey: From Idea to Reality',
+//         src: 'https://drive.google.com/file/d/13nLwAM7nj8tEsRTxB5uvrSqqFGRlqnWI/view?usp=drive_link',
+//         description: 'Description of Startup Journey, Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi, velit assumenda? Sed dolores quod natus fugiat dolor tenetur assumenda provident eaque voluptates officiis cumque possimus eos, voluptatum sunt. Corrupti, deleniti?',
+//         img: 'link_to_image_1'
+//     },
+//     {
+//         id: '2',
+//         title: 'Success Story: Overcoming Obstacles',
+//         src: 'https://drive.google.com/file/d/1SSXp5NMZYgjQH5aEAafRsgEsmiAEZmg8/view?usp=drive_link',
+//         description: 'Description of Success Story, Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi, velit assumenda? Sed dolores quod natus fugiat dolor tenetur assumenda provident eaque voluptates officiis cumque possimus eos, voluptatum sunt. Corrupti, deleniti?',
+//         img: 'link_to_image_2'
+//     },
+//     {
+//         id: '3',
+//         title: 'Learning from Failure: A Case Study',
+//         src: 'https://drive.google.com/file/d/12lPN6TwmqINzxkc9g1oBjyhHT6AgXvt6/view?usp=drive_link',
+//         description: 'Description of Learning from Failure, Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi, velit assumenda? Sed dolores quod natus fugiat dolor tenetur assumenda provident eaque voluptates officiis cumque possimus eos, voluptatum sunt. Corrupti, deleniti?',
+//         img: 'link_to_image_3'
+//     },
+//     {
+//         id: '4',
+//         title: 'Podcast: Expert Advice for Startups',
+//         src: 'https://drive.google.com/file/d/13nLwAM7nj8tEsRTxB5uvrSqqFGRlqnWI/view?usp=drive_link',
+//         description: 'Description of Podcast: Expert Advice, Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi, velit assumenda? Sed dolores quod natus fugiat dolor tenetur assumenda provident eaque voluptates officiis cumque possimus eos, voluptatum sunt. Corrupti, deleniti?',
+//         img: 'link_to_image_4'
+//     },
+//     {
+//         id: '5',
+//         title: 'Building a Dream Team',
+//         src: 'https://drive.google.com/file/d/1SSXp5NMZYgjQH5aEAafRsgEsmiAEZmg8/view?usp=drive_link',
+//         description: 'Description of Building a Dream Team, Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi, velit assumenda? Sed dolores quod natus fugiat dolor tenetur assumenda provident eaque voluptates officiis cumque possimus eos, voluptatum sunt. Corrupti, deleniti?',
+//         img: 'link_to_image_5'
+//     },
+//     {
+//         id: '6',
+//         title: 'Startup Mentorship: Guidance from the Best',
+//         src: 'https://drive.google.com/file/d/12lPN6TwmqINzxkc9g1oBjyhHT6AgXvt6/view?usp=drive_link',
+//         description: 'Description of Startup Mentorship, Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi, velit assumenda? Sed dolores quod natus fugiat dolor tenetur assumenda provident eaque voluptates officiis cumque possimus eos, voluptatum sunt. Corrupti, deleniti?',
+//         img: 'link_to_image_6'
+//     },
+//     {
+//         id: '7',
+//         title: 'Against All Odds: Inspiring Startup Stories',
+//         src: 'https://drive.google.com/file/d/13nLwAM7nj8tEsRTxB5uvrSqqFGRlqnWI/view?usp=drive_link',
+//         description: 'Description of Against All Odds, Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi, velit assumenda? Sed dolores quod natus fugiat dolor tenetur assumenda provident eaque voluptates officiis cumque possimus eos, voluptatum sunt. Corrupti, deleniti?',
+//         img: 'link_to_image_7'
+//     },
+//     {
+//         id: '8',
+//         title: 'Podcast: The Future of Startups',
+//         src: 'https://drive.google.com/file/d/1SSXp5NMZYgjQH5aEAafRsgEsmiAEZmg8/view?usp=drive_link',
+//         description: 'Description of Podcast: Future of Startups, Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi, velit assumenda? Sed dolores quod natus fugiat dolor tenetur assumenda provident eaque voluptates officiis cumque possimus eos, voluptatum sunt. Corrupti, deleniti?',
+//         img: 'link_to_image_8'
+//     },
+//     {
+//         id: '9',
+//         title: 'Success in Startup: A Practical Guide',
+//         src: 'https://drive.google.com/file/d/12lPN6TwmqINzxkc9g1oBjyhHT6AgXvt6/view?usp=drive_link',
+//         description: 'Description of Success in Startup, Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi, velit assumenda? Sed dolores quod natus fugiat dolor tenetur assumenda provident eaque voluptates officiis cumque possimus eos, voluptatum sunt. Corrupti, deleniti?',
+//         img: 'link_to_image_9'
+//     },
+//     {
+//         id: '10',
+//         title: 'Podcast: Marketing Strategies for Startups',
+//         src: 'https://drive.google.com/file/d/13nLwAM7nj8tEsRTxB5uvrSqqFGRlqnWI/view?usp=drive_link',
+//         description: 'Description of Podcast: Marketing Strategies, Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi, velit assumenda? Sed dolores quod natus fugiat dolor tenetur assumenda provident eaque voluptates officiis cumque possimus eos, voluptatum sunt. Corrupti, deleniti?',
+//         img: 'link_to_image_10'
+//     }
+//   ];
   
-  export default data;
+//   export default data;
   
